5f34642fb18d2baef180a856b369f16dc4db2408
nnnic <gea01441@nifty.com>
2019-06-09 10:48:22 +0900

    no message

--- test/hellomac2/hellomac2/ViewController.designer.cs
+++ test/hellomac2/hellomac2/ViewController.designer.cs
@@ -1,18 +1,26 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Xamarin Studio to store outlets and
+// This file has been generated automatically by Visual Studio to store outlets and
 // actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using Foundation;
+using System.CodeDom.Compiler;
 
 namespace hellomac2
 {
-    [Register("ViewController")]
-    partial class ViewController
-    {
-        void ReleaseDesignerOutlets()
-        {
-        }
-    }
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		AppKit.NSTextField label_clicked_outlet { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (label_clicked_outlet != null) {
+				label_clicked_outlet.Dispose ();
+				label_clicked_outlet = null;
+			}
+		}
+	}
 }
