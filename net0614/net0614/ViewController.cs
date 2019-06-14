3146b6b3fd3c1a96398640ae2a8beb35c432906b
nnnic <gea01441@nifty.com>
2019-06-14 21:40:39 +0900

    no message

---
+++ net0614/net0614/ViewController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using AppKit;
+using Foundation;
+
+namespace net0614
+{
+    public partial class ViewController : NSViewController
+    {
+        NSTimer m_timer;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Do any additional setup after loading the view.
+
+            image_outlet.Image = new NSImage("/Users/nnnic/Projects/unity-tutorial.png");
+
+            var i = 0;
+            m_timer = NSTimer.CreateRepeatingScheduledTimer(1f, (tm) => { Console.WriteLine("!" + i); i++; });
+
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+    }
+}
---
+++ net0614/net0614/ViewController.designer.cs
@@ -0,0 +1,26 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace net0614
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		AppKit.NSImageView image_outlet { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (image_outlet != null) {
+				image_outlet.Dispose ();
+				image_outlet = null;
+			}
+		}
+	}
+}
